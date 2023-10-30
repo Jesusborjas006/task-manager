f66a9662a9b579a083f372976f28ce18dfc7f61f
Jesusborjas006 <jesusbor006@gmail.com>
2023-10-30 17:14:57 -0500

    Conditionaly render login link only when user is on home page

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <nav className="flex items-center justify-between bg-blue-500 text-white py-4 px-6">
       <Logo />
-      <Link href="/login" className="text-xl">
-        Login
-      </Link>
+      {pathname === "/" && (
+        <Link href="/login" className="text-xl">
+          Login
+        </Link>
+      )}
     </nav>
   );
 };
